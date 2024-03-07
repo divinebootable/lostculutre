76f1f728ca51e7788486c4e9895416c22b09f063
dbootable@gmail.com <dbootable@gmail.com>
2024-03-07 18:34:13 +0100

    redux and axios setup

---
+++ src/serverConfig.jsx
@@ -0,0 +1,11 @@
+const URL = `http://localhost:8080/`;
+
+export default {
+  // AUTHENTICATION
+  SIGNUP: `${URL}signup`,
+  SIGNIN: `${URL}signin`,
+
+  //   REGISTRATION FOR COMPETITION
+
+  REGISTER: `${URL}register`,
+};
