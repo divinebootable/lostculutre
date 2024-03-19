b662bca508424cbf0e5f190c3d1c9af5ddc70fee
dbootable@gmail.com <dbootable@gmail.com>
2024-03-19 17:00:14 +0100

    card map function

--- src/components/contestantCard/card.jsx
+++ src/components/contestantCard/card.jsx
@@ -1,23 +1,20 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import './card.css';
 
 function ContestantCard({ item }) {
   return (
-    <Link>
-      <div className="container">
-        <div className="catCard">
-          <img src={item.img} alt="" />
-          <span className="name">{item.stageName}</span>
-          <button type="button" onClick={console.log(item.id)} className="button">
-            {' '}
-            Vote
-          </button>
-        </div>
+    <div className="container">
+      <div className="catCard">
+        <img src={item.img} alt="" />
+        <span className="name">{item.stageName}</span>
+        <button type="button" onClick={console.log(item)} className="button">
+          {' '}
+          Vote
+        </button>
       </div>
-    </Link>
+    </div>
   );
 }
 export default ContestantCard;
