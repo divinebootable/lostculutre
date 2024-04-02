0694401763b89f2ae9df273a4321b2644c17519f
Eweh-Emmanuel <emmanueleweh1@gmail.com>
2024-04-02 16:44:17 +0100

    'product'

--- src/sections/products/product-card.jsx
+++ src/sections/products/product-card.jsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 
 import { fCurrency } from 'src/utils/format-number';
@@ -14,6 +17,18 @@ import { ColorPreview } from 'src/components/color-utils';
 // ----------------------------------------------------------------------
 
 export default function ShopProductCard({ product }) {
+    const buttonStyle = {
+      backgroundColor: 'grey', // Set the desired background color here
+      color: 'white', // Set the text color
+    };
+    const [quantity, setQuantity] = useState(null); // State for the input field
+
+ 
+
+  const handleQuantityChange = (event) => {
+    const {value} = event.target;
+    setQuantity(value);
+  };
   const renderStatus = (
     <Label
       variant="filled"
@@ -61,6 +76,11 @@ export default function ShopProductCard({ product }) {
       {fCurrency(product.price)}
     </Typography>
   );
+  const renderSize = (
+    <Typography variant="subtitle1">
+      {product.size}
+    </Typography>
+  );
 
   return (
     <Card>
@@ -74,12 +94,28 @@ export default function ShopProductCard({ product }) {
         <Link color="inherit" underline="hover" variant="subtitle2" noWrap>
           {product.name}
         </Link>
+        <Stack direction="row" alignItems="center" justifyContent="space-between">
+          {renderSize}
+        </Stack>
 
         <Stack direction="row" alignItems="center" justifyContent="space-between">
           <ColorPreview colors={product.colors} />
           {renderPrice}
         </Stack>
+        <TextField
+          type="number"
+          label="Orange or Mtn number"
+          value={quantity}
+          onChange={handleQuantityChange}
+          InputProps={{
+            inputProps: { min: 1 },
+          }}
+        />
+        <Button size="medium" style={buttonStyle}>
+      Buy
+    </Button>
       </Stack>
+     
     </Card>
   );
 }
